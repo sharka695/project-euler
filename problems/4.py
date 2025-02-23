d399b62a26f67cda0f63886cf404d8c689568310
sharka695 <sharka695@users.noreply.github.com>
2025-02-23 02:17:05 -0800

    Finally found correct approach. findFirst() works. Others relied on confusion between unique factorizations and three-digit multiplicands.

--- problems/4.py
+++ problems/4.py
@@ -2,11 +2,25 @@ def factor(n):
     factor = 2
     factors = set()
     while factor <= n:
-      while n % factor == 0:
-        factors.add(factor)
-        n //= factor
-      factor += 1
+        while n % factor == 0:
+            factors.add(factor)
+            n //= factor
+        factor += 1
     return factors
+
+def uniqueFactorization(n):
+    factor = 2
+    factors = dict()
+    total = 0
+    while factor <= n:
+        while n % factor == 0:
+            if factor not in factors:
+                factors[factor] = 0
+            factors[factor] += 1
+            n //= factor
+            total += 1
+        factor += 1
+    return factors, total
 
 def isPalindrome(n):
     digits = []
@@ -22,7 +36,82 @@ def isPalindrome(n):
         j -= 1
     return True
 
-for i in range((1000 * 1000), (100 * 100), -1):
-    if isPalindrome(i) and len(factor(i)) > 1:
-        print(i)
-        break
+def digits(n):
+    count = 0
+    while n > 0:
+        count += 1
+        n //= 10
+    return count
+
+def allThreeDigit(nums):
+    for num in nums:
+        if digits(num) != 3:
+            return False
+    return True
+
+# def largestPalindromeFromThreeDigitFactors():
+#     for i in range((1000 * 1000), (100 * 100), -1):
+#         if isPalindrome(i):
+#             factors, total = uniqueFactorization(i)
+#             if total == 2:
+#                 #if allThreeDigit(factors.keys()):
+#                 print(i, uniqueFactorization(i))
+#                     #return i, uniqueFactorization(i)
+
+def palindromeFromThreeDigitPair(n):
+    if not isPalindrome(n):
+        return {}, 0
+    factor = 2
+    factors = dict()
+    total = 0
+    while factor <= n:
+        while n % factor == 0:
+            if digits(factor) == 3:
+                if factor not in factors:
+                    factors[factor] = 0
+                factors[factor] += 1
+            else:
+                return {}, 0
+            n //= factor
+            total += 1
+            # if total > 2:
+            #     return {}, 0
+        factor += 1
+    return factors, total
+
+
+def largestPalindromeFromThreeDigitFactors():
+    for i in range((1000 * 1000), (100 * 100), -1):
+        factors, total = palindromeFromThreeDigitPair(i)
+        if factors:
+            print(i, factors, total)
+
+
+def largestPalindromeFromThreeDigitFactors():
+    for i in range((1000 * 1000), (100 * 100), -1):
+        if not isPalindrome(i):
+            continue
+        factors = factor(i)
+        if allThreeDigit(factors):
+            print(i, factors)
+
+def checkForRest():
+    for i in range(353, 1000):
+        for j in range(283, 1000):
+            product = i * j
+            if isPalindrome(product):
+                print(product)
+
+def findFirst():
+    biggest = 0
+    big_i = 0
+    big_j = 0
+    for i in range(999, 99, -1):
+        for j in range(999, 99, -1):
+            product = i * j
+            if isPalindrome(product):
+                if product > biggest:
+                    biggest = product
+                    big_i, big_j = i, j
+    print(big_i, big_j)
+    return biggest

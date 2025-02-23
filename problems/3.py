580a46ac498367e556551dacf9fb2caafeba510d
sharka695 <sharka695@users.noreply.github.com>
2025-02-23 02:22:07 -0800

    Copied over uniqueFactorization() from 4.py.

--- problems/3.py
+++ problems/3.py
@@ -1,3 +1,16 @@
 num = 600_851_475_143
-def largest_prime_factor(n):
-    
+def uniqueFactorization(n):
+    factor = 2
+    factors = dict()
+    total = 0
+    while factor <= n:
+        while n % factor == 0:
+            if factor not in factors:
+                factors[factor] = 0
+            factors[factor] += 1
+            n //= factor
+            total += 1
+        factor += 1
+    return factors, total
+
+uniqueFactorization(num)

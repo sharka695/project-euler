6f88b484c10e99d229a30f7d6fae517303146175
sharka695 <6828420+sharka695@users.noreply.github.com>
2025-02-22 21:45:49 -0800

    Fixed syntactic issues.

--- problems/4.py
+++ problems/4.py
@@ -4,7 +4,7 @@ def factor(n):
     while n:
       while n % factor == 0:
         factors.add(factor)
-        n // = factor
+        n //= factor
       factor += 1
     return factors
 
@@ -15,7 +15,7 @@ def isPalindrome(n):
         n //= 10
     i = 0
     j = len(digits) - 1
-    while i < j
+    while i < j:
         if digits[i] != digits[j]:
             return False
         i += 1
@@ -24,4 +24,5 @@ def isPalindrome(n):
 
 for i in range((1000 * 1000), (100 * 100), -1):
     if isPalindrome(i) and len(factor(i)) > 1:
-        return i
+        print(i)
+        break

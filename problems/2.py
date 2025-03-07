b865a7b0207e574b6de56371084eff1259d0379d
sharka695 <sharka695@users.noreply.github.com>
2025-03-06 23:24:24 -0800

    Update 2.py. Add writeup.

--- problems/2.py
+++ problems/2.py
@@ -1,3 +1,11 @@
+""" Dynamic programming offers a very nice way to quickly calculate Fibonacci numbers by
+    calculating each Fibonacci number only once, using the results of only the previous two
+    calculations, each stored in a variable (a and b).
+
+    While calculating each Fibonacci number, one can filter for even-ness by dividing it by 2,
+    checking if the remainder is 0, then adding it to the total if it's even (i.e., n is even if n mod 2 = 0).
+"""
+
 def fib(n):
   a, b = 1, 2
   for i in range(n - 1):

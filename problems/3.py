4e3c71cdeb9ca67fa7cccca07e3ba2941fabac95
sharka695 <sharka695@users.noreply.github.com>
2025-03-07 00:18:58 -0800

    Update 3.py. Add writeup.

--- problems/3.py
+++ problems/3.py
@@ -1,3 +1,11 @@
+""" Factorization can be done by dividing the number by candidate factors. If there's no remainder,
+    then the candidate is a factor. This factor can then be "removed" from the number by division.
+    Doing so repeatedly yields a unique prime factorization, in keeping with the fundamental theorem of arithmetic.
+    Given the unique prime factorization, one can find the largest factor by comparing each prime. If they're small
+    enough, you might just be able to compare them by looking at them. You can also use the max function, which will do
+    this comparison for you.
+"""
+
 num = 600_851_475_143
 def uniqueFactorization(n):
     factor = 2
